f196fa5ecc8b8a8047f8c8d38b641353658649d5
xzho2604 <erikxiangzhou@gmail.com>
2019-03-05 16:09:35 +1100

    version 2 app.py

--- app.py
+++ app.py
@@ -1,6 +1,33 @@
-#!/usr/bin/env python
+# -*- coding:utf8 -*-
+# !/usr/bin/env python
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-import urllib
+
+# Updated in July 2018 to reflect Dialogflow v2 changes for request/response
+# Author - Naresh Ganatra
+# http://youtube.com/c/NareshGanatra
+
+
+from __future__ import print_function
+#from future.standard_library import install_aliases
+#install_aliases()
+
+from urllib.parse import urlparse, urlencode
+from urllib.request import urlopen, Request
+from urllib.error import HTTPError
+
 import json
 import os
 
@@ -17,39 +44,123 @@ def webhook():
     req = request.get_json(silent=True, force=True)
 
     print("Request:")
+    # commented out by Naresh
     print(json.dumps(req, indent=4))
 
-    res = makeWebhookResult(req)
+    res = processRequest(req)
 
     res = json.dumps(res, indent=4)
-    print(res)
+    #print(res)
     r = make_response(res)
     r.headers['Content-Type'] = 'application/json'
     return r
 
-def makeWebhookResult(req):
-    if req.get("result").get("action") != "shipping.cost":
+
+def processRequest(req):
+    print ("here I am....")
+    print ("starting processRequest...",req.get("queryResult").get("action"))
+    if req.get("queryResult").get("action") != "yahooWeatherForecast":
+        print ("Please check your action name in DialogFlow...")
         return {}
-    result = req.get("result")
+    print("111111111111")
+    baseurl = "https://query.yahooapis.com/v1/public/yql?"
+    print("1.5 1.5 1.5")
+    yql_query = makeYqlQuery(req)
+    print ("2222222222")
+    if yql_query is None:
+        return {}
+    yql_url = baseurl + urlencode({'q': yql_query}) + "&format=json"
+    print("3333333333")
+    print (yql_url)
+    result = urlopen(yql_url).read()
+    data = json.loads(result)
+    #for some the line above gives an error and hence decoding to utf-8 might help
+    #data = json.loads(result.decode('utf-8'))
+    print("44444444444")
+    print (data)
+    res = makeWebhookResult(data)
+    return res
+
+
+def makeYqlQuery(req):
+    result = req.get("queryResult")
     parameters = result.get("parameters")
-    zone = parameters.get("bank-name")
+    city = parameters.get("geo-city")
+    if city is None:
+        return None
+    return "select * from weather.forecast where woeid in (select woeid from geo.places(1) where text='" + city + "')"
 
-    cost = {'Andhra Bank':'6.85%', 'Allahabad Bank':'6.75%', 'Axis Bank':'6.5%', 'Bandhan bank':'7.15%', 'Bank of Maharashtra':'6.50%', 'Bank of Baroda':'6.90%', 'Bank of India':'6.60%', 'Bharatiya Mahila Bank':'7.00%', 'Canara Bank':'6.50%', 'Central Bank of India':'6.60%', 'City Union Bank':'7.10%', 'Corporation Bank':'6.75%', 'Citi Bank':'5.25%', 'DBS Bank':'6.30%', 'Dena Bank':'6.80%', 'Deutsche Bank':'6.00%', 'Dhanalakshmi Bank':'6.60%', 'DHFL Bank':'7.75%', 'Federal Bank':'6.70%', 'HDFC Bank':'5.75% to 6.75%', 'Post Office':'7.10%', 'Indian Overseas Bank':'6.75%', 'ICICI Bank':'6.25% to 6.9%', 'IDBI Bank':'6.65%', 'Indian Bank':'4.75%', 'Indusind Bank':'6.85%', 'J&K Bank':'6.75%', 'Karnataka Bank':'6.50 to 6.90%', 'Karur Vysya Bank':'6.75%', 'Kotak Mahindra Bank':'6.6%', 'Lakshmi Vilas Bank':'7.00%', 'Nainital Bank':'7.90%', 'Oriental Bank of Commerce':'6.85%', 'Punjab National Bank':'6.75%', 'Punjab and Sind Bank':'6.4% to 6.80%', 'Saraswat bank':'6.8%', 'South Indian Bank':'6% to 6.75%', 'State Bank of India':'6.75%', 'Syndicate Bank':'6.50%', 'Tamilnad Mercantile Bank Ltd':'6.90%', 'UCO bank':'6.75%', 'United Bank Of India':'6%', 'Vijaya Bank':'6.50%', 'Yes Bank':'7.10%'}
 
-    speech = "The interest rate of " + zone + " is " + str(cost[zone])
+def makeWebhookResult(data):
+    print ("starting makeWebhookResult...")
+    query = data.get('query')
+    if query is None:
+        return {}
+
+    result = query.get('results')
+    if result is None:
+        return {}
+
+    channel = result.get('channel')
+    if channel is None:
+        return {}
+
+    item = channel.get('item')
+    location = channel.get('location')
+    units = channel.get('units')
+    if (location is None) or (item is None) or (units is None):
+        return {}
+
+    condition = item.get('condition')
+    if condition is None:
+        return {}
+
+    # print(json.dumps(item, indent=4))
+
+    speech = "Today the weather in " + location.get('city') + ": " + condition.get('text') + \
+             ", And the temperature is " + condition.get('temp') + " " + units.get('temperature')
+
     print("Response:")
     print(speech)
+    #Naresh
     return {
-        "speech": speech,
-        "displayText": speech,
-        #"data": {},
-        #"contextOut": [],
-        "source": "BankRates"
+
+    "fulfillmentText": speech,
+     "source": "Yahoo Weather"
     }
 
+
+@app.route('/test', methods=['GET'])
+def test():
+    return  "Hello there my friend !!"
+
+
+@app.route('/static_reply', methods=['POST'])
+def static_reply():
+    speech = "Hello there, this reply is from the webhook !! "
+    string = "You are awesome !!"
+    Message ="this is the message"
+
+    my_result =  {
+
+    "fulfillmentText": string,
+     "source": string
+    }
+
+    res = json.dumps(my_result, indent=4)
+
+    r = make_response(res)
+
+    r.headers['Content-Type'] = 'application/json'
+    return r
+
+
+
 if __name__ == '__main__':
+
+
     port = int(os.getenv('PORT', 5000))
 
-    print ("Starting app on port %d" %(port))
+    print("Starting app on port %d" % port)
 
     app.run(debug=True, port=port, host='0.0.0.0')
