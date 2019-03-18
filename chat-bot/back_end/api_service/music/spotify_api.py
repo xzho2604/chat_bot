7e1acc91f41fc4634dcc2b9df41da5d0c97736d3
PakoMY <shi0907hnu@163.com>
2019-03-18 19:32:17 +1100

    music api demo include

---
+++ chat-bot/back_end/api_service/music/spotify_api.py
@@ -0,0 +1,50 @@
+
+import sys
+import spotipy
+
+''' shows the albums and tracks for a given artist.
+'''
+def  get_access_token(filename):
+    file = open(filename)
+    try:
+        token = file.read()
+    finally:
+        file.close()
+    return token
+
+def get_artist(name):
+    results = sp.search(q='artist:' + name, type='artist')
+    items = results['artists']['items']
+    if len(items) > 0:
+        return items[0]
+    else:
+        return None
+
+
+def show_artist_albums(artist):
+    albums = []
+    results = sp.artist_albums(artist['id'], album_type='album')
+    albums.extend(results['items'])
+    while results['next']:
+        results = sp.next(results)
+        albums.extend(results['items'])
+    seen = set()  # to avoid dups
+    albums.sort(key=lambda album: album['name'].lower())
+    for album in albums:
+        name = album['name']
+        if name not in seen:
+            print((' ' + name))
+            seen.add(name)
+
+
+if __name__ == '__main__':
+    filename = './web-api-auth/authorization_code/auth_token.txt'
+    token = get_access_token(filename)
+    sp = spotipy.Spotify(auth = token)
+    name = 'michael jackson'
+
+    artist = get_artist(name)
+    if artist:
+        show_artist_albums(artist)
+    else:
+        print("Can't find that artist")
